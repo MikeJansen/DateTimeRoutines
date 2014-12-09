b31e761051900bf9eda373e86261f87e4641d455
Diego Barros <diego@heyboyo.com>
2014-12-09 18:02:41 +1100

    Added support for the common date component separators: "/", ".", "-", " ". Handle missing separator where it makes sense to be able to parse a date. e.g. 12Feb2030. Added support for fixed: yyyymmdd.

--- Test/Program.cs
+++ Test/Program.cs
@@ -67,9 +67,18 @@ namespace Cliver
         {
             ArrayList dates = new ArrayList();
             dates.Add(new DTTest(@"Member since:  	10-Feb-2008", new DateTime(2008, 2, 10, 0, 0, 0)));
+				dates.Add(new DTTest(@"Member since:  	10/Feb/2008", new DateTime(2008, 2, 10, 0, 0, 0)));
+				dates.Add(new DTTest(@"Member since:  	10 Feb 2008", new DateTime(2008, 2, 10, 0, 0, 0)));
+				dates.Add(new DTTest(@"Member since:  	10.Feb.2008", new DateTime(2008, 2, 10, 0, 0, 0)));
+				dates.Add(new DTTest(@"Member since:  	10Feb2008", new DateTime(2008, 2, 10, 0, 0, 0)));
+
+				dates.Add(new DTTest(@"see at 12-31-2007 14:16:32", new DateTime(2007, 12, 31, 14, 16, 32)));
+				dates.Add(new DTTest(@"see at 12/31/2007 14:16:32", new DateTime(2007, 12, 31, 14, 16, 32)));
+				dates.Add(new DTTest(@"see at 12 31 2007 14:16:32", new DateTime(2007, 12, 31, 14, 16, 32)));
+				dates.Add(new DTTest(@"see at 12.31.2007 14:16:32", new DateTime(2007, 12, 31, 14, 16, 32)));
+
             dates.Add(new DTTest(@"Last Update: 18:16 11 Feb '08 ", new DateTime(2008, 2, 11, 18, 16, 0)));
             dates.Add(new DTTest(@"date	Tue, Feb 10, 2008 at 11:06 AM", new DateTime(2008, 2, 10, 11, 06, 0)));
-            dates.Add(new DTTest(@"see at 12/31/2007 14:16:32", new DateTime(2007, 12, 31, 14, 16, 32)));
             dates.Add(new DTTest(@"sack finish 14:16:32 November 15 2008, 1-144 app", new DateTime(2008, 11, 15, 14, 16, 32)));
             dates.Add(new DTTest(@"Genesis Message - Wed 04 Feb 08 - 19:40", new DateTime(2008, 2, 4, 19, 40, 0)));
             dates.Add(new DTTest(@"The day 07/31/07 14:16:32 is ", new DateTime(2007, 7, 31, 14, 16, 32)));
@@ -78,12 +87,25 @@ namespace Cliver
             dates.Add(new DTTest(@"5th November, 1994, 8:15:30 pm", new DateTime(1994, 11, 5, 20, 15, 30)));
             dates.Add(new DTTest(@"7 boxes January 31 , 14:16:32.", new DateTime(DateTime.Now.Year, 1, 31, 14, 16, 32)));
             dates.Add(new DTTest(@"the blue sky of Sept  30th  2008 14:16:32", new DateTime(2008, 9, 30, 14, 16, 32)));
-            dates.Add(new DTTest(@" e.g. 1997-07-16T19:20:30+01:00", new DateTime(1997, 7, 16, 19, 20, 30)));
-            dates.Add(new DTTest(@"Apr 1st, 2008 14:16:32 tufa 6767", new DateTime(2008, 4, 1, 14, 16, 32)));
+
+				dates.Add(new DTTest(@" e.g. 1997-07-16T19:20:30+01:00", new DateTime(1997, 7, 16, 19, 20, 30)));
+				dates.Add(new DTTest(@" e.g. 1997 07 16T19:20:30+01:00", new DateTime(1997, 7, 16, 19, 20, 30)));
+				dates.Add(new DTTest(@" e.g. 1997/07/16T19:20:30+01:00", new DateTime(1997, 7, 16, 19, 20, 30)));
+				dates.Add(new DTTest(@" e.g. 1997.07.16T19:20:30+01:00", new DateTime(1997, 7, 16, 19, 20, 30)));
+
+				dates.Add(new DTTest(@" e.g. 20081120 04:23:13", new DateTime(2008, 11, 20, 4, 23, 13)));
+
+				dates.Add(new DTTest(@"Apr 1st, 2008 14:16:32 tufa 6767", new DateTime(2008, 4, 1, 14, 16, 32)));
             dates.Add(new DTTest(@"wait for 07/31/07 14:16:32", new DateTime(2007, 7, 31, 14, 16, 32)));
             dates.Add(new DTTest(@"later 12.31.08 and before 1.01.09", new DateTime(2008, 12, 31, 0, 0, 0)));
-            dates.Add(new DTTest(@"Expires: Sept  30th  2008 14:16:32", new DateTime(2008, 9, 30, 14, 16, 32)));
-            dates.Add(new DTTest(@"Offer expires Apr 1st, 2007, 14:16:32", new DateTime(2007, 4, 1, 14, 16, 32)));
+
+			   dates.Add(new DTTest(@"Expires: Sept  30th  2008 14:16:32", new DateTime(2008, 9, 30, 14, 16, 32)));
+				dates.Add(new DTTest(@"Expires: Sept 30 2008 14:16:32", new DateTime(2008, 9, 30, 14, 16, 32)));
+				dates.Add(new DTTest(@"Expires: Sept-30-2008 14:16:32", new DateTime(2008, 9, 30, 14, 16, 32)));
+				dates.Add(new DTTest(@"Expires: Sept/30/2008 14:16:32", new DateTime(2008, 9, 30, 14, 16, 32)));
+				dates.Add(new DTTest(@"Expires: Sept.30.2008 14:16:32", new DateTime(2008, 9, 30, 14, 16, 32)));
+            
+			   dates.Add(new DTTest(@"Offer expires Apr 1st, 2007, 14:16:32", new DateTime(2007, 4, 1, 14, 16, 32)));
             dates.Add(new DTTest(@"Expires  14:16:32 January 31.", new DateTime(DateTime.Now.Year, 1, 31, 14, 16, 32)));
             dates.Add(new DTTest(@"Expires  14:16:32 January 31-st.", new DateTime(DateTime.Now.Year, 1, 31, 14, 16, 32)));
             dates.Add(new DTTest(@"Expires 23rd January 2010.", new DateTime(2010, 1, 23, 0, 0, 0)));
